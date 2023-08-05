5a1a1f30911d8f9a7178f9e5f63722ad3782dbe2
barabulko <barabulko@yahoo.com>
2023-08-05 11:25:15 +0500

    added comments

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -1,3 +1,10 @@
+/*В данном участке кода можно применить принцип открытости/закрытости (Open/Closed Principle).
+Вместо использования switch-case для определения оператора, можно создать отдельные классы для каждой математической операции,
+которые реализуют интерфейс Operation, имеющий метод double performOperation(double num1, double num2).
+Класс CalculatorModel будет оперировать не конкретными операторами, а объектами, реализующими этот интерфейс.
+Таким образом, при добавлении новой операции необходимо будет только добавить новый класс,
+реализующий интерфейс Operation, без изменения существующего кода.*/
+
 public class CalculatorModel {
     public double performOperation(double num1, double num2, String operator) {
         switch (operator) {
